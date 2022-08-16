621cc521d01fcc337c018ddb695bcf7e39b8413c
Biser Sirakov <Biser.Sirakov@pxpfinancial.com>
2022-08-17 00:20:25 +0300

    client: add ngx-org-chart

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { TeamMember } from './models/team-member.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  nodes: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const res = this.http
+      .get<TeamMember>('http://localhost:1010/api/team')
+      .subscribe((team) => {
+        this.nodes = [this.mapTeamMemberToOrganizationNode(team)];
+      });
+  }
+
+  mapTeamMemberToOrganizationNode(teamMember: TeamMember) {
+    const node: { name: string; title: string; childs: any[] } = {
+      name: teamMember.name,
+      title: teamMember.email,
+      childs: [],
+    };
+
+    teamMember.employees?.forEach((employee) => {
+      node.childs?.push(this.mapTeamMemberToOrganizationNode(employee));
+    });
+
+    return node;
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxOrgChartModule } from 'ngx-org-chart';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, NgxOrgChartModule],
   providers: [],
   bootstrap: [AppComponent],
 })
